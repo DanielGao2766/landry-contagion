7d55132f6c551df36a05485c266afd23205f21dc
Dan <danielgao2766@gmail.com>
2026-09-23 11:45:57 -0400

    Chore: Add Daniel's Draft

--- src/contagion_draft.jl
+++ src/contagion_draft.jl
@@ -1,4 +1,22 @@
 using Graphs
+using CSV
+using Graphs
+"""
+Terminal Command: 
+using Pkg
+Pkg.add(["CSV", "DataFrames", "Graphs"])
+"""
+
+# Graph Sample taken from https://arxiv.org/pdf/1312.097
+edges = CSV.read("graph_sample.csv", DataFrame)
+
+nodes = CSV.read("node_states_sample.csv", DataFrame)
+
+# Build a sample graph
+graph = SimpleDiGraph(nrow(nodes))
+for row in eachrow(edges)
+    add_edge!(graph, row.source, row.target)
+end
 
 function contagion(graph, kernel, node_states)
     # Go through every node in the graph
@@ -14,20 +32,27 @@ function contagion(graph, kernel, node_states)
     return new_states
 end
 
-# Run the file 
-function main()
-    local_node_states = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+# # Run the file 
+# function main()
+#     local_node_states = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+#     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-    for i in 1:20
-        local_node_states = contagion(
+#     for i in 1:20
+#         local_node_states = contagion(
+#             graph,
+#             kernel,
+#             local_node_states
+#         )
+#     end
+
+#     println(local_node_states)
+# end
+
+  for i in 1:20
+        nodes_df.state = contagion(
             graph,
             kernel,
-            local_node_states
+            nodes_df.state
         )
     end
-
-    println(local_node_states)
-end
-
-main()
+println(nodes_df.state)
